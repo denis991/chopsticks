b1d7c1eb932ac5f699869f51f4fd3588adaffe45
Denis Gutsulak <d.99113@gmail.com>
2023-06-03 17:07:49 +0300

    header good

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 import * as flsFunctions from "./modules/functions.js";
 import {addEventListenerToHeaderPhone} from "./modules/eventListeners.js";
 // import * as age from './modules/age';
-// import {timeOfLife} from './modules/age';
+// import * as timeOfLife from './modules/age';
 
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 /* (i) необходимо для корректного отображения webp из css  */
 flsFunctions.isWebp();
+// flsFunctions.mobile();
+flsFunctions.browserCheck();
+
 addEventListenerToHeaderPhone();
 
 // timeOfLife(birthStr);
