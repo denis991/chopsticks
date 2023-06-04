bb60605dea58f41851f2079e2a738f44c1c2237d
Denis Gutsulak <d.99113@gmail.com>
2023-06-04 23:09:17 +0300

    good about our product

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 import * as flsFunctions from "./modules/functions.js";
 import {addEventListenerToHeaderPhone} from "./modules/eventListeners.js";
-// import * as age from './modules/age';
+
 // import * as timeOfLife from './modules/age';
 
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 /* (i) необходимо для корректного отображения webp из css  */
 flsFunctions.isWebp();
-// flsFunctions.mobile();
 flsFunctions.browserCheck();
+flsFunctions.calculateBorderRadius();
 
 addEventListenerToHeaderPhone();
 
