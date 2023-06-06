d85125442235538fd2f58da0b992be91e5be39a7
Denis Gutsulak <d.99113@gmail.com>
2023-06-06 17:44:12 +0300

    gallery button position + _pop-pup

--- src/js/modules/eventListeners.js
+++ src/js/modules/eventListeners.js
@@ -20,10 +20,41 @@ const addEventListenerCloseNavbarMobil = () => {
     headerMobil?.classList.toggle('active');
   });
 };
+//=================== POPUP ===================
+function openModal() {
+  const nameInput = document.getElementById('nam');
+  const phoneInput = document.getElementById('phone');
 
+  console.log('Name:', nameInput.value);
+  console.log('Phone:', phoneInput.value);
+
+  const modalContainer = document.querySelector('.modal-container');
+  modalContainer.classList.add('active');
+
+	  // Очистка значений инпутов
+		nameInput.value = '';
+		phoneInput.value = '';
+}
+
+function closeModal() {
+  const modalContainer = document.querySelector('.modal-container');
+  modalContainer.classList.remove('active');
+}
+
+
+const addEventListenerPopPup = () => {
+const button = document.getElementById('request');
+button.addEventListener('click', openModal);
+
+const closeButton = document.querySelector('.modal-close-button');
+closeButton.addEventListener('click', closeModal);
+}
+
+//=================== вызов функций ===================
 const addEventListenerToHeaderPhone = () => {
 
   addEventListenerOpenNavbarMobile();
   addEventListenerCloseNavbarMobil();
+	addEventListenerPopPup()
 };
 export { addEventListenerToHeaderPhone  };
